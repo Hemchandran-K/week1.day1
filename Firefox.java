2ec5c7d88e75f4aa9c64a083089d8f8d911476aa
Hemchandran-K <109062675+Hemchandran-K@users.noreply.github.com>
2022-07-14 16:40:28 +0530

    Add files via upload

---
+++ Firefox.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Firefox {
+	public static void main(String[] args) {
+		String name = "Firefox";
+		float version = 100.2f;
+		boolean visible = true;
+		int year = 1998;
+		char logo = 'M';
+		System.out.println("This is my first script");
+
+	}
+
+}
